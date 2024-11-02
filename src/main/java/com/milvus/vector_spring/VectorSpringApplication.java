77fdef89d9ec28878c464c16d6e25518252f29a0
o3ojunseok <o3ojunseok@gmail.com>
2024-11-02 23:28:10 +0900

    Feat: setting

---
+++ src/main/java/com/milvus/vector_spring/VectorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.milvus.vector_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorSpringApplication.class, args);
+	}
+
+}
