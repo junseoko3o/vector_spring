d4319533cf95e91b4d22832a9ee515ea8e57e0cd
o3ojunseok <o3ojunseok@gmail.com>
2024-11-09 00:20:46 +0900

    Feat: User Entity

---
+++ src/main/java/com/milvus/vector_spring/user/User.java
@@ -0,0 +1,11 @@
+package com.milvus.vector_spring.user;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+}
