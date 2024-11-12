dcfcb43a9d77a2eb2c2c38b15f2f5cab23f0c538
o3ojunseok <o3ojunseok@gmail.com>
2024-11-12 11:17:00 +0900

    Feat: user entity builder

--- src/main/java/com/milvus/vector_spring/user/User.java
+++ src/main/java/com/milvus/vector_spring/user/User.java
@@ -1,11 +1,43 @@
 package com.milvus.vector_spring.user;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "user_id")
+    private String email;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "login_at")
+    private Date loginAt;
+
+    @Builder
+    public User(long id, String email, String userName, String password) {
+        this.id = id;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public User updateLoginAt(Date loginAt) {
+        this.loginAt = loginAt;
+        return this;
+    }
 }
