ec9f2ebbe261af3e08dddd5323f1f5c961bdb4e4
o3ojunseok <o3ojunseok@gmail.com>
2024-11-14 11:12:26 +0900

    Feat: findAllUser

--- src/main/java/com/milvus/vector_spring/user/UserRepository.java
+++ src/main/java/com/milvus/vector_spring/user/UserRepository.java
@@ -2,5 +2,9 @@ package com.milvus.vector_spring.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserId(Long userId);
 }
--- src/main/java/com/milvus/vector_spring/user/UserService.java
+++ src/main/java/com/milvus/vector_spring/user/UserService.java
@@ -3,8 +3,19 @@ package com.milvus.vector_spring.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findOneUser(Long id) {
+        return userRepository.findByUserId(id);
+    }
 }
