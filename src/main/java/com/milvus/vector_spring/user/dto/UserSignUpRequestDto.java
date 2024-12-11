355672583453c9ed7adbc66552e9f47929db2fbc
junseoko3o <github.yam117@passinbox.com>
2024-12-11 23:52:48 +0900

    Fix: sign up not null

--- src/main/java/com/milvus/vector_spring/user/dto/UserSignUpRequestDto.java
+++ src/main/java/com/milvus/vector_spring/user/dto/UserSignUpRequestDto.java
@@ -2,6 +2,7 @@ package com.milvus.vector_spring.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserSignUpRequestDto {
     @NotBlank
+    @NotNull
     private String username;
 
     @NotBlank
+    @NotNull
     @Email(message = "이메일 형식 이어야 한다.")
     private String email;
 
     @NotBlank
+    @NotNull
     @Pattern(
         regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
         message = "비밀번호는 숫자, 문자, 특수문자 1개 이상 ")
