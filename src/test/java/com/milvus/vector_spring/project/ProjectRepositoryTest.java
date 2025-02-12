c14573274054d5e20b56f8421ce48f40a543d34e
junseoko3o <github.yam117@passinbox.com>
2025-02-12 21:54:50 +0900

    Feat: ProjectRepositoryTest

---
+++ src/test/java/com/milvus/vector_spring/project/ProjectRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.milvus.vector_spring.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectRepositoryTest {
+
+    @Test
+    void findProjectByKey() {
+    }
+}
