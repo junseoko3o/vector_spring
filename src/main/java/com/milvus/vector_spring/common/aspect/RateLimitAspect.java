0853a36e81c75cbac18040c92066cfe43063ac8d
junseoko3o <github.yam117@passinbox.com>
2025-04-16 00:10:08 +0900

    Fix: rate limit 클라이언트 실제 ip

--- src/main/java/com/milvus/vector_spring/common/aspect/RateLimitAspect.java
+++ src/main/java/com/milvus/vector_spring/common/aspect/RateLimitAspect.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 @Aspect
 @Component
@@ -22,23 +21,34 @@ public class RateLimitAspect {
     private HttpServletRequest request;
 
     private static final int MAX_REQUESTS = 20;
-    private static final LocalDateTime TIME_WINDOW = LocalDateTime.now().plusHours(24);
+    private static final Duration DURATION = Duration.ofHours(24);
 
     @Around("@annotation(rateLimit)")
     public Object around(ProceedingJoinPoint joinPoint, RateLimit rateLimit) throws Throwable {
-        String ip = request.getRemoteAddr();
-        String key = "rate_limit: " + ip;
+        String ip = getClientIp();
+        String key = "rate_limit:ip:" + ip;
 
-        String currentCount = redisTemplate.opsForValue().get(key);
-        if (currentCount != null && Integer.parseInt(currentCount) >= MAX_REQUESTS) {
+        String currentCountStr = redisTemplate.opsForValue().get(key);
+        int currentCount = currentCountStr == null ? 0 : Integer.parseInt(currentCountStr);
+
+        if (currentCount >= MAX_REQUESTS) {
             throw new RuntimeException("횟수 초과");
         }
-        redisTemplate.opsForValue().increment(key, 1);
 
-        if(currentCount == null) {
-            redisTemplate.expire(key, Duration.ofHours(24));
+        Long newCount = redisTemplate.opsForValue().increment(key, 1);
+        if (newCount != null && newCount == 1L) {
+            redisTemplate.expire(key, DURATION);
         }
 
         return joinPoint.proceed();
     }
+
+    private String getClientIp() {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null) {
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
+    }
 }
+
