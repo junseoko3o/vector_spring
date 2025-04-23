e5670db62ce537e03d08e5d9b013ce07cc01d1a1
junseoko3o <github.yam117@passinbox.com>
2025-04-23 21:22:52 +0900

    Fix: error messages

--- src/main/java/com/milvus/vector_spring/common/Const.java
+++ src/main/java/com/milvus/vector_spring/common/Const.java
@@ -3,4 +3,5 @@ package com.milvus.vector_spring.common;
 public class Const {
     public static final String USER_ID = "userId";
     public static final String CONTENT_ID = "contentId";
+    public static final String PROJECT_ID = "projectId";
 }
