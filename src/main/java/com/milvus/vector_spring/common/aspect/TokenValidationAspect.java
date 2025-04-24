bcd3ced51fc04ac1f2d6c6dcdc145c608468daee
junseoko3o <github.yam117@passinbox.com>
2025-04-24 22:59:56 +0900

    Fix: TokenValidationAspect refac

--- src/main/java/com/milvus/vector_spring/common/aspect/TokenValidationAspect.java
+++ src/main/java/com/milvus/vector_spring/common/aspect/TokenValidationAspect.java
@@ -18,37 +18,17 @@ public class TokenValidationAspect {
     @Around("@annotation(requireToken)")
     public Object validateToken(ProceedingJoinPoint joinPoint, RequireToken requireToken) {
         try {
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-            String token = request.getHeader("Authorization");
-
-            if (token == null || token.isEmpty()) {
-                throw new CustomException(ErrorStatus._TOKEN_NOT_FOUND);
-            }
-
-            if (!token.startsWith("Bearer ")) {
-                throw new CustomException(ErrorStatus._INVALID_TOKEN_FORMAT);
-            }
-
-            token = token.substring(7);
+            HttpServletRequest request = getCurrentHttpRequest();
+            String token = extractToken(request);
 
             if (!isValidToken(token)) {
                 throw new CustomException(ErrorStatus._INVALID_TOKEN);
             }
 
             Object[] args = joinPoint.getArgs();
-
             if (args.length > 0 && args[0] instanceof String) {
                 args[0] = token;
             }
-//          Cookie[] cookies;
-//        String tokens = getAccessTokenFromCookies(request.getCookies());
-//        if (cookies != null) {
-//            for (Cookie cookie : cookies) {
-//                if (COOKIE_NAME.equals(cookie.getName())) {
-//                    return cookie.getValue();
-//                }
-//            }
-//        }
 
             return joinPoint.proceed(args);
         } catch (CustomException e) {
@@ -58,8 +38,40 @@ public class TokenValidationAspect {
         }
     }
 
+    private HttpServletRequest getCurrentHttpRequest() {
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attrs == null) {
+            throw new CustomException(ErrorStatus._INTERNAL_SERVER_ERROR);
+        }
+        return attrs.getRequest();
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || header.isEmpty()) {
+            throw new CustomException(ErrorStatus._TOKEN_NOT_FOUND);
+        }
+
+        if (!header.startsWith("Bearer ")) {
+            throw new CustomException(ErrorStatus._INVALID_TOKEN_FORMAT);
+        }
+
+        return header.substring(7);
+    }
+
     private boolean isValidToken(String token) {
         // TODO: 실제 토큰 검증 로직 구현
         return token != null && !token.isEmpty();
     }
+
+    //          Cookie[] cookies;
+//        String tokens = getAccessTokenFromCookies(request.getCookies());
+//        if (cookies != null) {
+//            for (Cookie cookie : cookies) {
+//                if (COOKIE_NAME.equals(cookie.getName())) {
+//                    return cookie.getValue();
+//                }
+//            }
+//        }
 }
