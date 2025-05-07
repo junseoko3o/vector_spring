e6d060829ff34d90e93795c7eecc30acbc15fad6
junseoko3o <github.yam117@passinbox.com>
2025-05-08 00:09:08 +0900

    Fix: custom exception

--- src/main/java/com/milvus/vector_spring/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/milvus/vector_spring/common/exception/GlobalExceptionHandler.java
@@ -14,27 +14,26 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiResponse<String>> handleRuntimeException(RuntimeException e) {
+    public ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException e) {
         String errorMessage = e.getMessage() != null ? e.getMessage() : "Runtime exception occurred";
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse<>("500", errorMessage));
+                .body(new ApiResponse("500", errorMessage));
     }
 
-
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ApiResponse<String>> handleCustomException(CustomException e) {
+    public ResponseEntity<ApiResponse> handleCustomException(CustomException e) {
         BaseCode errorCode = e.getBaseCode();
         return handleExceptionInternal(errorCode);
     }
 
-    private ResponseEntity<ApiResponse<String>> handleExceptionInternal(BaseCode errorCode) {
+    private ResponseEntity<ApiResponse> handleExceptionInternal(BaseCode errorCode) {
         return ResponseEntity.status(errorCode.getReasonHttpStatus().getHttpStatus())
                 .body(ApiResponse.fail(errorCode));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<String>> handleBindException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ApiResponse> handleBindException(MethodArgumentNotValidException ex) {
         String errorCodes = ex.getBindingResult().getAllErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
@@ -42,16 +41,16 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ApiResponse<>("400", errorCodes));
+                .body(new ApiResponse("400", errorCodes));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<String>> handleAllExceptions(Exception ex) {
+    public ResponseEntity<ApiResponse> handleAllExceptions(Exception ex) {
         String errorMessage = ex.getMessage() != null ? ex.getMessage() : "An unexpected server error occurred";
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse<>(
+                .body(new ApiResponse(
                         "500",
                         errorMessage
                 ));
