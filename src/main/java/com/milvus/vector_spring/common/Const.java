2214f1f5695d7c29b9d1897064bd584855570889
junseoko3o <github.yam117@passinbox.com>
2025-05-13 23:38:54 +0900

    Feat: projectKey header

--- src/main/java/com/milvus/vector_spring/common/Const.java
+++ src/main/java/com/milvus/vector_spring/common/Const.java
@@ -4,4 +4,5 @@ public class Const {
     public static final String USER_ID = "userId";
     public static final String CONTENT_ID = "contentId";
     public static final String PROJECT_ID = "projectId";
+    public static final String PROJECT_KEY = "projectKey";
 }
